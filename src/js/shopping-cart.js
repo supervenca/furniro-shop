a2dce6295b8e1c87560245e3d34fae6157f9a108
Ventseslava <supervenca@yandex.ru>
2024-06-09 20:18:31 +0300

    fixed shopping-cart bg and icons

--- src/js/shopping-cart.js
+++ src/js/shopping-cart.js
@@ -1,14 +1,14 @@
-const shoppingCart = document.querySelector('.shopping-cart');
+const shoppingCart = document.querySelector('section');
 const cartBtn = document.querySelector('.cart');
 const closeBtn = document.querySelector('.shopping-cart__icon');
 
 function openShoppingCart() {
     if (shoppingCart.style.display = 'none'){
-        shoppingCart.style.display = 'grid';
+        shoppingCart.style.display = 'block';
     }
 }
 function closeShoppingcart() {
-    if (shoppingCart.style.display = 'grid'){
+    if (shoppingCart.style.display = 'block'){
         shoppingCart.style.display = 'none';
     }
 }
